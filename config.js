7aa1b2465768882f0af1fff9e578992754ee52e7
Rakhmad <rakhmadbudiono211@gmail.com>
2021-05-23 13:47:05 +0700

    use geth to implement private ethereum

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   ETH_NETWORK: "http://127.0.0.1:8545",
   PRIVATE_KEY:
-    "fc5cc99a5e7ad2f1f2a13cee46a05c5d85df468220dedfc033f959a245ae58b3",
+    "0x38a60cce176f31e28395da89570344c8ab33276ad51d41169ac9bc0233107beb",
   UPLOAD_API: "http://localhost:3002",
-  CHAIN_ID: 1337,
+  CHAIN_ID: 1234,
   IPFS_HOST: "ipfs.infura.io",
   IPFS_PORT: 5001,
   IPFS_API: "https://ipfs.io/ipfs",
